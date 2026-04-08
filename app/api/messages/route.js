1c580280496bfedaca9585224cbd646d9fb4da24
wwwwwangx <919278709@qq.com>
2026-04-08 11:53:11 +0800

    1

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -3,7 +3,7 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: false,
 });
 
 const SESSION_ID = "default";
